60c42ceb6e486b97455270e8e03c08ff091a9ffa
maskikuy <kproject305@gmail.com>
2025-10-20 09:52:54 +0700

    membuat method get

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+let mysql = require('mysql2');
+const app = express();
+const PORT = 3000;
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'mahasiswa',
+    port: 3308
+});
+
+db.connect((err) => {
+    if (err) {
+        console.error('Error connecting to MySQL:'+ err.stack);
+        return;
+    }
+    console.log('Connection successfully!');
+});
+
+app.get('/api/users', (req, res) => {
+    db.query('SELECT * FROM mahasiswa', (err, results) => {
+        if (err) {
+            console.error('Error executing query: ' + err.stack);
+            res.status(500).send('Error fetching users');
+            return;
+        }
+        res.json(results);
+    })
+})
+
