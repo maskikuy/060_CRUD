88fbe5e3cb004170781071e5e76de5ef0c929cac
maskikuy <kproject305@gmail.com>
2025-10-20 10:10:41 +0700

    menambahkan method put

--- index.js
+++ index.js
@@ -70,4 +70,21 @@ app.post('/api/users', (req, res) => {
             res.status(201).json({ message: 'User created successfully'});
         }
     );
+});
+
+app.put('/api/users/:id', (req, res) => {
+    const userId = req.params.id;
+    const { nama, nim, kelas } = req.body;
+    db.query(
+        'UPDATE mahasiswa SET nama = ?, nim = ?, kelas = ? WHERE id = ?',
+        [nama, nim, kelas, userId],
+        (err, results) => {
+            if (err) {
+                console.error(err);
+                return res.status(500).json({ message: 'Database Error' });
+            }
+            res.json({ message: 'User updated successfully' });
+        }
+    );
+});
     
